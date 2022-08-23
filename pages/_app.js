77c76cbbbd828cda10309b7f7114b274d81d0e13
LPX <info@shapeshift.one>
2022-08-24 00:55:36 +0900

    fix(path)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'nextra-theme-docs/style.css'
-import '/main.css'
+import '../main.css'
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || (page => page)

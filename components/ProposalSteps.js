f266caa0efcdfebec442aa37d2c28c4a499fb4f3
LPX <info@shapeshift.one>
2022-09-21 08:38:23 +0900

    update(gov-ws)

--- components/ProposalSteps.js
+++ components/ProposalSteps.js
@@ -5,70 +5,21 @@ import Stack from '@mui/material/Stack';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import Check from '@mui/icons-material/Check';
-import SettingsIcon from '@mui/icons-material/Settings';
-import GroupAddIcon from '@mui/icons-material/GroupAdd';
-import VideoLabelIcon from '@mui/icons-material/VideoLabel';
+import ModelTrainingIcon from '@mui/icons-material/ModelTraining';
 import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import GradingIcon from '@mui/icons-material/Grading';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import HowToVoteIcon from '@mui/icons-material/HowToVote';
 
-
-const QontoStepIconRoot = styled('div')(({ theme, ownerState }) => ({
-  color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#eaeaf0',
-  display: 'flex',
-  height: 22,
-  alignItems: 'center',
-  ...(ownerState.active && {
-    color: '#784af4',
-  }),
-  '& .QontoStepIcon-completedIcon': {
-    color: '#784af4',
-    zIndex: 1,
-    fontSize: 18,
-  },
-  '& .QontoStepIcon-circle': {
-    width: 8,
-    height: 8,
-    borderRadius: '50%',
-    backgroundColor: 'currentColor',
-  },
-}));
-
-function QontoStepIcon(props) {
-  const { active, completed, className } = props;
-
-  return (
-    <QontoStepIconRoot ownerState={{ active }} className={className}>
-      {completed ? (
-        <Check className="QontoStepIcon-completedIcon" />
-      ) : (
-        <div className="QontoStepIcon-circle" />
-      )}
-    </QontoStepIconRoot>
-  );
-}
-
-QontoStepIcon.propTypes = {
-  /**
-   * Whether this step is active.
-   * @default false
-   */
-  active: PropTypes.bool,
-  className: PropTypes.string,
-  /**
-   * Mark the step as completed. Is passed to child components.
-   * @default false
-   */
-  completed: PropTypes.bool,
-};
 
 const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.alternativeLabel}`]: {
-    top: 22,
+    top: 40,
   },
   [`&.${stepConnectorClasses.active}`]: {
     [`& .${stepConnectorClasses.line}`]: {
       backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(186, 59, 38) 100%)',
     },
   },
   [`&.${stepConnectorClasses.completed}`]: {
@@ -90,20 +41,20 @@ const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
   zIndex: 1,
   color: '#fff',
-  width: 50,
-  height: 50,
+  width: 80,
+  height: 80,
   display: 'flex',
   borderRadius: '50%',
   justifyContent: 'center',
   alignItems: 'center',
   ...(ownerState.active && {
     backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(237, 76, 49) 50%, rgb(186, 59, 38) 100%)',
     boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
   }),
   ...(ownerState.completed && {
     backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(237, 76, 49) 50%, rgb(186, 59, 38) 100%)',
   }),
 }));
 
@@ -111,10 +62,10 @@ function ColorlibStepIcon(props) {
   const { active, completed, className } = props;
 
   const icons = {
-    1: <SettingsIcon />,
-    2: <GroupAddIcon />,
-    3: <VideoLabelIcon />,
-    4: <VideoLabelIcon />,
+    1: <ModelTrainingIcon sx={{ fontSize: '2.5rem' }} />,
+    2: <GradingIcon sx={{ fontSize: '2.5rem' }} />,
+    3: <ThermostatIcon sx={{ fontSize: '2.5rem' }} />,
+    4: <HowToVoteIcon sx={{ fontSize: '2.5rem' }} />,
   };
 
   return (
@@ -142,11 +93,11 @@ ColorlibStepIcon.propTypes = {
   icon: PropTypes.node,
 };
 
-const steps = ['Phase 1', 'Phase 2', 'Phase 3', 'Phase 4'];
+const steps = ['Phase 1 - Discussion', 'Phase 2 - Formalization', 'Phase 3 - Signal', 'Phase 4 - On-Chain Vote'];
 
-export default function CustomizedSteppers() {
+export default function CustomizedSteppers(props) {
   return (
-      <Stepper alternativeLabel activeStep={1} connector={<ColorlibConnector />}>
+      <Stepper alternativeLabel activeStep={props.step} connector={<ColorlibConnector />}>
         {steps.map((label) => (
           <Step key={label}>
             <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
